466b85d9f32a28fb5ca02c70154bf7141b5b60f1
Uri Simhony <uris@pmtsit.com>
2019-12-15 19:24:23 +0200

    v0.0.12

--- src/models/document-block.ts
+++ src/models/document-block.ts
@@ -2,6 +2,7 @@ import { Expose, Type } from 'class-transformer';
 import { TemplateBlock } from './template-block';
 import { Post } from './post';
 import { Quantity } from './quantity';
+import { Document as DocumentEntity } from './document';
 import { WeatherForecast } from './weather-forecast';
 
 export class DocumentBlock {
@@ -11,6 +12,8 @@ export class DocumentBlock {
   public templateBlock: TemplateBlock;
   @Type(() => Post)
   public posts: Post[];
+  @Type(() => DocumentEntity)
+  public document?: DocumentEntity;
   @Type(() => WeatherForecast)
   @Expose({ name: 'weather_forecast' })
   public weatherForecast?: WeatherForecast;
